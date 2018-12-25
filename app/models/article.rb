f574d15b2dd4fc5a347260d4a012c65dd41eec62
daijinru <jeocat@163.com>
2018-12-26 00:49:29 +0800

    refactor comments html and its delete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    has_many :comments, dependent: :delete_all
+    has_many :comments, dependent: :destroy
     validates :title, presence: true, length: {minimum: 5}
 end

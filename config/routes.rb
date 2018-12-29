47070b901a32e8080ea6164fb82a44daa3c8ee56
daijinru <jeocat@163.com>
2018-12-30 02:02:44 +0800

    feat: http auth basic replace with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles do
     resources :comments
   end
 
-  root 'welcome#index'
+  root 'home#index'
 end

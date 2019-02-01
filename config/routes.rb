7c019d32782a0625fa5da9aca6e6e5250060190c
daijinru <jeocat@163.com>
2019-02-02 01:34:43 +0800

    refactor: add jquery remove coffee

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  devise_for :users
-  get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles do
-    resources :comments
-  end
-
-  root 'home#index'
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :articles do
+        resources :comments
+    end
+    get 'welcome/index'
+    root 'welcome#index'
 end
